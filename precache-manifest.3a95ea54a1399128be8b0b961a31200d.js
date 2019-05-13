727547bb4776ed07a6ee33fe7b46f6d23f352d4e
franz101 <fkrekeler@gmail.com>
2019-05-13 13:24:12 +0200

    first commit

---
+++ precache-manifest.3a95ea54a1399128be8b0b961a31200d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac56dd759396d409f18a5e103f9fdfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2c98a4ead14af456d5",
+    "url": "/static/css/2.3cec64ce.chunk.css"
+  },
+  {
+    "revision": "b7a0ad601d2d4e2b23e8",
+    "url": "/static/css/main.48843e1e.chunk.css"
+  },
+  {
+    "revision": "6c2c98a4ead14af456d5",
+    "url": "/static/js/2.15b1491d.chunk.js"
+  },
+  {
+    "revision": "b7a0ad601d2d4e2b23e8",
+    "url": "/static/js/main.d4cc6173.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
