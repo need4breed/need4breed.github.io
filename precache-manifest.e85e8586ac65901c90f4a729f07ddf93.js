ea0901da0f450f7a222291cc703cfd313a82c308
need4breed <50580014+need4breed@users.noreply.github.com>
2020-05-10 23:06:13 +0200

    Add files via upload

---
+++ precache-manifest.e85e8586ac65901c90f4a729f07ddf93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a942c54b0f8c9834b772a930247b42b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b3c8b6dcea8da451504",
+    "url": "/static/css/2.454b31d8.chunk.css"
+  },
+  {
+    "revision": "0f2b5b452cd9c948faa7",
+    "url": "/static/css/main.63532655.chunk.css"
+  },
+  {
+    "revision": "4b3c8b6dcea8da451504",
+    "url": "/static/js/2.371038d7.chunk.js"
+  },
+  {
+    "revision": "0f2b5b452cd9c948faa7",
+    "url": "/static/js/main.fd56916b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
